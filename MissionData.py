73896d9df6df638cfcdb8b24e55960f7962f9695
Graeme Cruickshank <gcruickshank1985@gmail.com>
2021-05-01 16:55:47 +0200

    eng/PR-10 Refactor into a streamlined structure
    Refactored to a slightly more streamlined structure.
    Added new classes for carrier data, mission data and commodity informations.
    Cleaned up method names and some method logics.

---
+++ MissionData.py
@@ -0,0 +1,56 @@
+class MissionData:
+
+    def __init__(self, info_dict=None):
+        """
+        Class represents a mission object as returned from the database.
+        """
+        if not info_dict:
+            info_dict = dict()
+
+        # TODO: Some of these will be NULL because that is what we get back from the SQL database, we should convert
+        #  those to python types
+
+        # TODO: What do we get back from db.fetchone(), is it a dictionary object?
+
+        self.carrier_name = info_dict.get('carrier', None)
+        self.carrier_identifier = info_dict.get('cid', None)
+        self.channel_id = info_dict.get('channelid', None)
+        self.commodity = info_dict.get('commodity', None)
+        self.mission_type = info_dict.get('missiontype', None)
+        self.system = info_dict.get('system', None)
+        self.station = info_dict.get('station', None)
+        self.profit = info_dict.get('profit', None)
+        self.pad_size = info_dict.get('pad', None)
+        self.demand = info_dict.get('demand', None)
+        self.rp_text = info_dict.get('rp_text', None)
+        self.reddit_post_id = info_dict.get('reddit_post_id', None)
+        self.reddit_post_url = info_dict.get('reddit_post_url', None)
+        self.reddit_comment_id = info_dict.get('reddit_comment_id', None)
+        self.reddit_comment_url = info_dict.get('reddit_comment_url', None)
+        self.discord_alert_id = info_dict.get('discord_alert_id', None)
+
+    def to_dictionary(self):
+        """
+        Formats the mission data into a dictionary for easy access.
+
+        :returns: A dictionary representation for the mission data.
+        :rtype: dict
+        """
+        response = {}
+        for key, value in vars(self).items():
+            if value is not None:
+                response[key] = value
+        return response
+
+    def __str__(self):
+        """
+        Overloads str to return a readable object
+
+        :rtype: str
+        """
+        return 'MissionData: CarrierName:{0.carrier_name} CarrierIdentifier:{0.carrier_identifier} ' \
+               'DiscordChannelID:{0.channel_id} Commodity:{0.commodity} ' \
+               'MissingType:{0.mission_type} System:{0.system} Station:(0.station} Profit:{0.profit}' \
+               'Pad:{0.pad} Demand:{0.demand} RpText:{0.rp_text} RedditPostId:{0.reddit_post_id}' \
+               'RedditPostUrl:{0.reddit_post_url} RedditCommentId:{0.reddit_comment_id}' \
+               'RedditCommentUrl:{0.reddit_comment_url} DiscordAlertId:{0.discord_alert_id}'.format(self)
