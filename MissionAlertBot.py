137ae9c38ef61a1d7db327b54974b3dd92b76cbc
cimspin <80884597+cimspin@users.noreply.github.com>
2021-07-29 23:01:46 +0100

    Fix 191

--- MissionAlertBot.py
+++ MissionAlertBot.py
@@ -1253,12 +1253,17 @@ async def done(ctx, carrier_name_search_term, rp=None):
         # fill in some info for messages
         desc_msg = f"> {rp}\n\n" if rp else ""
 
+        # check if it's Wine, in which case it went to the booze cruise channel
+        if mission_data.commodity.title() == "Wine":
+            channel = bot.get_channel(wine_alerts_id)
+        else:
+            channel = bot.get_channel(trade_alerts_id)
+
         # delete Discord trade alert
         if mission_data.discord_alert_id and mission_data.discord_alert_id != 'NULL':
             try:  # try in case it's already been deleted, which doesn't matter to us in the slightest but we don't
                 # want it messing up the rest of the function
                 discord_alert_id = mission_data.discord_alert_id
-                channel = bot.get_channel(trade_alerts_id)
                 msg = await channel.fetch_message(discord_alert_id)
                 await msg.delete()
             except:
@@ -1358,7 +1363,11 @@ async def complete(ctx):
                     # delete Discord trade alert
                     if mission_data.discord_alert_id and mission_data.discord_alert_id != 'NULL':
                         try:
-                            channel = bot.get_channel(trade_alerts_id)
+                            # check if it's Wine, in which case it went to the booze cruise channel
+                            if mission_data.commodity.title() == "Wine":
+                                channel = bot.get_channel(wine_alerts_id)
+                            else:
+                                channel = bot.get_channel(trade_alerts_id)
                             msg = await channel.fetch_message(mission_data.discord_alert_id)
                             await msg.delete()
                         except:
