e3c8dc05998d3dc3fe897447acefa5469f42c7ab
cimspin <80884597+cimspin@users.noreply.github.com>
2021-08-25 08:49:18 +0100

    Update MissionAlertBot.py

--- MissionAlertBot.py
+++ MissionAlertBot.py
@@ -3419,7 +3419,9 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.CommandNotFound):
         await ctx.send("**Invalid command.**")
     elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('**Please include all required parameters.** Use m.help <command> for details.')
+        await ctx.send("**Sorry, that didn't work**.\n• Check you've included all required arguments. Use `m.help <command>` for details."
+                       "\n• If using quotation marks, check they're opened *and* closed, and are in the proper place.\n• Check quotation"
+                       " marks are of the same type, i.e. all straight or matching open/close smartquotes.")
     elif isinstance(error, commands.MissingPermissions):
         await ctx.send('**You must be a Carrier Owner to use this command.**')
     else:
