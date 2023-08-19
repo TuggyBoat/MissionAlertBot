373a2f30c3a6875c94595ba7987d5776e0506f54
TuggyBoat <39076583+TuggyBoat@users.noreply.github.com>
2023-08-18 23:04:12 -0500

    Update GeneralCommands.py
    Added bot restriction to /nominate command

--- ptn/missionalertbot/botcommands/GeneralCommands.py
+++ ptn/missionalertbot/botcommands/GeneralCommands.py
@@ -22,7 +22,7 @@ from ptn.missionalertbot.classes.Views import MissionCompleteView
 from ptn.missionalertbot._metadata import __version__
 import ptn.missionalertbot.constants as constants
 from ptn.missionalertbot.constants import bot, bot_command_channel, bot_dev_channel, cmentor_role, certcarrier_role, \
-    admin_role, dev_role, trade_alerts_channel, mod_role, cpillar_role, bot_spam_channel
+    admin_role, dev_role, trade_alerts_channel, mod_role, cpillar_role, bot_spam_channel, bot_role
 
 # local modules
 from ptn.missionalertbot.database.database import backup_database, find_carrier, find_mission, _is_carrier_channel, \
@@ -504,13 +504,13 @@ class GeneralCommands(commands.Cog):
             print(f"{interaction.user} tried to nominate themselves for Community Pillar :]")
             return await interaction.response.send_message("You can't nominate yourself! But congrats on the positive self-esteem :)", ephemeral=True)
 
-        #Skip nominating Cpillar|Cmentor|Council|Mod
-        for avoid_role in [cpillar_role(), cmentor_role(), admin_role(), mod_role()]:
+        #Skip nominating Cpillar|Cmentor|Council|Mod|Bot
+        for avoid_role in [cpillar_role(), cmentor_role(), admin_role(), mod_role(), bot_role()]:
             if user.get_role(avoid_role):
                 print(f"{interaction.user} tried to nominate a Cpillar|Cmentor|Council|Mod : {user.name}")
                 return await interaction.response.send_message(
                     (f"You can't nominate an existing <@&{cpillar_role()}>,"
-                    f" <@&{cmentor_role()}>, <@&{admin_role()}> or <@&{mod_role()}>."
+                    f" <@&{cmentor_role()}>, <@&{admin_role()}>, <@&{mod_role()}> or bot."
                     " But we appreciate your nomination attempt!"),
                     ephemeral=True)
 
